0986027109db0455746487ed0bb15b6a1a01e12c
TheRenat <therenat@mail.ru>
2022-08-25 23:00:18 +0300

    add model

---
+++ Task List/Models/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  Task List
+//
+//  Created by Renat Ibragimov on 25.08.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date =  Date
+    @Persisted var isComplete = false
+}
